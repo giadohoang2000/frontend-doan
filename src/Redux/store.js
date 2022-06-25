af3f5be510f3af41cf534993f8cb8d1f13954a4a
giadohoang2000 <giadohoang2000@gmail.com>
2022-06-25 23:37:24 +0700

    fe

---
+++ src/Redux/store.js
@@ -0,0 +1,57 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./Reducers/ProductReducers";
+import { cartReducer } from "./Reducers/CartReducers";
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from "./Reducers/UserReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+});
+
+const cartItemFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+//login
+
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+  //shipping address
+const shippingAddressFromLocalStorage = localStorage.getItem("shippingAdress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
+const initialState = {
+  cart: {
+    cartItems: cartItemFromLocalStorage,
+    shippingAddress: shippingAddressFromLocalStorage
+  },
+  userLogin: { userInfo: userInfoFromLocalStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
